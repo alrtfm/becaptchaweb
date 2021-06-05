19cce46f8d0ecc9d81f9a3e3ca9f7cf114acc9cd
alrtfm <85386372+alrtfm@users.noreply.github.com>
2021-06-05 10:21:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -506,5 +506,5 @@ def ambos_sensores():
     return  render_template("acelerometro.html")
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080, debug=True)
+    app.run()
     #app.run(host="0.0.0.0", port=8080, debug=True, ssl_context='adhoc')
